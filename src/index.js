f6e4a7b0d9addb297463f8953096f36c357e7a51
Tetiana Bronitska <tanya_nest@ukr.net>
2021-09-30 01:16:46 +0300

    Added change theme

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-import './sass/main.scss';
+import itemTmp from './tmp.hbs';
+import menuItems from './menu.json'
+
+const menuContainer = document.querySelector('.js-menu');
+const cardMarkup = createMenuMarkup(menuItems);
+
+menuContainer.insertAdjacentHTML('beforeend', cardMarkup);
+
+function createMenuMarkup(menuItems) {
+    return itemTmp(menuItems);
+}
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const inputToggle = document.querySelector('#theme-switch-toggle');
+const body = document.querySelector('body');
+inputToggle.addEventListener('change', onInput);
+
+function onInput(e) {
+      if (e.target.checked) {
+        body.classList.add(Theme.DARK)
+        body.classList.remove(Theme.LIGHT)
+        localStorage.setItem('theme', 'dark');
+    }
+    else {
+        body.classList.add(Theme.LIGHT)
+         body.classList.remove(Theme.DARK)
+         localStorage.setItem('theme', 'light');
+    }
+}
+
+populateInput();
+
+function populateInput() {
+    if (localStorage.getItem('theme') === 'dark') {
+    body.classList.add(Theme.DARK);
+    inputToggle.checked = true;
+    } else {
+    body.classList.add(Theme.LIGHT);
+    inputToggle.checked = false;
+    }
+}
